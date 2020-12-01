969484d222342af83c02c86c1345a9547516583f
Rodrigo Russo <68329584+RodrigoGaluppo@users.noreply.github.com>
2020-12-01 14:28:54 +0000

    v11
    v11

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  document.getElementById('root')
+    <App />,document.getElementById('root')
 );
 
 serviceWorker.register();
-reportWebVitals();
